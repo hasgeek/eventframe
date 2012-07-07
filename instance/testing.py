54ec4e16d3cb9065bc341099d12dfb337a8efa61
Kiran Jonnalagadda <jace@pobox.com>
2012-07-07 11:38:34 +0530

    Testing setup (with 0 tests currently)

---
+++ instance/testing.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#: Site title
+SITE_TITLE = 'HasGeek Eventframe'
+#: Site id (for network bar)
+SITE_ID = 'events'
+#: Admin domains. The first is considered primary
+ADMIN_HOSTS = ['eventframe.hasgeek.com', 'efdev.hasgeek.in']
+#: Using SSL?
+USE_SSL = True
+#: Database backend
+SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+#: Secret key
+SECRET_KEY = 'make this something random'
+#: Timezone
+TIMEZONE = 'Asia/Calcutta'
+#: LastUser server
+LASTUSER_SERVER = 'https://auth.hasgeek.com/'
+#: LastUser client id
+LASTUSER_CLIENT_ID = ''
+#: LastUser client secret
+LASTUSER_CLIENT_SECRET = ''
+#: Path to site themes (must be an absolute path)
+THEME_PATHS = '/'
+#: Mail settings
+#: MAIL_FAIL_SILENTLY : default True
+#: MAIL_SERVER : default 'localhost'
+#: MAIL_PORT : default 25
+#: MAIL_USE_TLS : default False
+#: MAIL_USE_SSL : default False
+#: MAIL_USERNAME : default None
+#: MAIL_PASSWORD : default None
+#: DEFAULT_MAIL_SENDER : default None
+MAIL_FAIL_SILENTLY = False
+MAIL_SERVER = 'localhost'
+DEFAULT_MAIL_SENDER = ('HasGeek', 'test@example.com')
+#: Logging: recipients of error emails
+ADMINS = []
+#: Log file
+LOGFILE = 'error.log'
