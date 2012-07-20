329c81c45638dfef81e8b78341c254762454c424
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-07-20 09:34:09 +0530

    UI tweaks

--- eventframe/views/index.py
+++ eventframe/views/index.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template
-from eventframe import app, eventapp
+from eventframe import app
+from eventframe.models.website import Website
+from eventframe.views.login import lastuser
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    websites = Website.query.order_by('title').all()
+    return render_template('index.html', websites=websites, admin=lastuser.has_permission('siteadmin'))
