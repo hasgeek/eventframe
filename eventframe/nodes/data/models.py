e5a145cebb47f3abce2a132a48a2a9b1d8b27119
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-01-08 09:50:44 +0530

    List access API for data nodes.

--- eventframe/nodes/data/models.py
+++ eventframe/nodes/data/models.py
@@ -16,3 +16,23 @@ class Data(NodeMixin, Node):
     def import_from(self, data):
         super(NodeMixin, self).import_from(data)
         self.data = data['data']
+
+    # Facilitate easier access to data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __iter__(self):
+        return self.data.iterkeys()
+
+    def __contains__(self, item):
+        return item in self.data
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
