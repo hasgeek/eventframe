96a6fa1c8ac82499b37022c6bf47b5d17d22066b
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-10-12 14:43:07 +0530

    1. Switch to Baseframe's .html.jinja2 templates (see hasgeek/baseframe#166) 2. Rename all app templates to use .html.jinja2 extension

--- eventframe/views/errorhandlers.py
+++ eventframe/views/errorhandlers.py
@@ -9,18 +9,18 @@ from eventframe.nodes import get_website
 @get_website
 def error404(website, e):
     theme = get_theme(website.theme)
-    return render_theme_template(theme, '404.html'), 404
+    return render_theme_template(theme, '404.html.jinja2'), 404
 
 
 @eventapp.errorhandler(403)
 @get_website
 def error403(website, e):
     theme = get_theme(website.theme)
-    return render_theme_template(theme, '403.html'), 403
+    return render_theme_template(theme, '403.html.jinja2'), 403
 
 
 @eventapp.errorhandler(500)
 @get_website
 def error500(website, e):
     theme = get_theme(website.theme)
-    return render_theme_template(theme, '500.html'), 500
+    return render_theme_template(theme, '500.html.jinja2'), 500
